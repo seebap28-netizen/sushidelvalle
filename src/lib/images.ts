dd31bbe9b8648fafae1a12411b48b2e67f77bd2e
seebap28-netizen <seebap28@gmail.com>
2026-09-14 17:35:09 -0300

    Mantiene cambio de relleno junto a relleno extra en extras.

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -76,6 +76,8 @@ const byId: Record<string, string> = {
   "ex-prot-cam-sal": "/photos/extra-prot-cam-sal.png",
   "ex-relleno": "/photos/avocado-roll.png",
   "ex-relleno-champi": "/photos/burger-ex-champi.png",
+  "ex-cambio-relleno": "/photos/avocado-roll.png",
+  "ex-cambio-relleno-champi": "/photos/burger-ex-champi.png",
   "rel-morron": "/photos/burger-ex-morron.png",
   "rel-aceituna": "/photos/burger-ex-aceituna.png",
   "prot-champ": "/photos/burger-ex-champi.png",
